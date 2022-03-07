e734d250567315c6853b35782cff3a1bcd71c264
unknown <ilir.neziri98@gmail.com>
2022-03-07 11:45:34 +0100

    folio design

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
     var typed_strings = $(".typed-text").text();
     var typed = new Typed(".typed-text-output", {
       strings: typed_strings.split(", "),
-      typeSpeed: 100,
+      typeSpeed: 120,
       backSpeed: 20,
       smartBackspace: false,
       loop: true,
